5e005faf590583a2663857a2fe742ab70849fc96
cmedrano <cristian.ariel.medrano@gmail.com>
2026-02-03 17:44:52 -0300

    add resend

--- WorkerSendMails.Console/Program.cs
+++ WorkerSendMails.Console/Program.cs
@@ -1,28 +1,67 @@
 ﻿using NLog;
+using Resend;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text;
 
 class Program
 {
-    static void Main()
+    //static void Main()
+    //{
+    //    LogManager.Setup()
+    //        .LoadConfigurationFromFile("NLog.config");
+
+    //    Console.WriteLine("comienza el job");
+    //    var logger = LogManager.GetLogger("FinalJobLogger");
+
+    //    try
+    //    {
+    //        logger.Info("Job iniciado");
+    //        logger.Info("Procesando datos...");
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        logger.Error(ex, "Error en el job");
+    //        throw;
+    //    }
+    //    finally
+    //    {
+    //        Console.WriteLine("termina el job");
+    //        logger.Info("Job finalizado"); // 📬 mail
+    //        LogManager.Shutdown();
+    //    }
+    //}
+    static async Task Main()
     {
-        LogManager.Setup()
-            .LoadConfigurationFromFile("NLog.config");
+            var apiKey = Environment.GetEnvironmentVariable("RESEND_API_KEY");
 
-        var logger = LogManager.GetLogger("FinalJobLogger");
+            if (string.IsNullOrWhiteSpace(apiKey))
+            {
+                Console.WriteLine("❌ Falta RESEND_API_KEY");
+                return;
+            }
 
-        try
-        {
-            logger.Info("Job iniciado");
-            logger.Info("Procesando datos...");
-        }
-        catch (Exception ex)
-        {
-            logger.Error(ex, "Error en el job");
-            throw;
-        }
-        finally
-        {
-            logger.Info("Job finalizado"); // 📬 mail
-            LogManager.Shutdown();
+            using var http = new HttpClient();
+            http.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", apiKey);
+
+            var payload = new
+            {
+                from = "onboarding@resend.dev",
+                to = new[] { "andresfriasmedrib@gmail.com" },
+                subject = "📬 Test Resend desde consola",
+                html = "<strong>Mail enviado correctamente desde consola .NET</strong>"
+            };
+
+            var json = JsonSerializer.Serialize(payload);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await http.PostAsync("https://api.resend.com/emails", content);
+
+            var body = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(response.IsSuccessStatusCode
+                ? "✅ Mail enviado correctamente"
+                : $"❌ Error: {body}");
         }
     }
-}
